1f6dfbf7406fd2cb6af966e5ee0f9e030d955d31
zyouhua <zyouhua@gmail.com>
2014-02-06 13:21:31 +0800

    update

--- 20140101/account/AccountService.h
+++ 20140101/account/AccountService.h
@@ -5,16 +5,6 @@ namespace std {
 	class AccountService : public PropertySink
 	{
 	public:
-		public ErrorCode_ _createAccount(const char * nAccountName, const char * nNickname, const char * nPassward)
-		{
-			uint hashName_ = GenerateId._runTableId(nAccountName);
-			AccountConfig accountConfig_ = __singleton<AccountConfig>._instance();
-			uint accountMgrCount_ = accountConfig_._getAccountMgrCount();
-			uint accountMgrIndex_ = hashName_ % accountMgrCount_;
-			AccountMgr accountMgr_ = mAccountMgrs[accountMgrIndex_];
-			return accountMgr_._createAccount(nAccountName, nNickname, nPassward);
-		}
-
 		template<class __t>
 		void _serialize(__t * nT);
 		const char * _streamName();
